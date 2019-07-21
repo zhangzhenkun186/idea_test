9f0bbec373cfa38b2da454a81e1f35fa22938525
zhang <zhangzhenkun@163.com>
2019-07-22 01:11:24 +0800

    第二次提交代码到本地仓库

--- src/main/java/com/zzk/HelloWorld.java
+++ src/main/java/com/zzk/HelloWorld.java
@@ -32,6 +32,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+            System.out.println("测试");
         }
 
 
