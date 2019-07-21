43a77a8d6047f4da03fec650dd8656d8a6a90d11
zhangzhenkun186 <zhangzhenkun186@163.com>
2019-07-22 01:42:54 +0800

    测试修改

--- src/main/java/com/zzk/User.java
+++ src/main/java/com/zzk/User.java
@@ -1,27 +1,38 @@
 package com.zzk;
 
-/**
- * 快捷键生成set和get,以及构造方法：alt+insert
- */
 public class User {
 
-    private   String   name;
+	private String name;
 
-    private   int    age;
+    private int   age;
+    
+    private int   sex;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public int getAge() {
+		return age;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getSex() {
+		return sex;
+	}
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+    
+    public static void main(String[] args) {
+		System.out.println("测试冲突");
+	}
 }
