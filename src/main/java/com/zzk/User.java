1d9695b0e796e3388b7a8f0b1afb466cdc8319c1
zhang <zhangzhenkun@163.com>
2019-07-22 00:12:05 +0800

    第一次提交代码到本地仓库

---
+++ src/main/java/com/zzk/User.java
@@ -0,0 +1,27 @@
+package com.zzk;
+
+/**
+ * 快捷键生成set和get,以及构造方法：alt+insert
+ */
+public class User {
+
+    private   String   name;
+
+    private   int    age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
